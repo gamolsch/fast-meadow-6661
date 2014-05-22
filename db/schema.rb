c596450a404f4fbc07ad3b1b1833debaac2c8854
George Amolsch <gamolsch@gmail.com>
2014-05-22 13:43:17 -0500

    chore(users): adds password column to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140522131406) do
+ActiveRecord::Schema.define(version: 20140522183322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,9 +31,18 @@ ActiveRecord::Schema.define(version: 20140522131406) do
 
   create_table "storages", force: true do |t|
     t.string   "name"
-    t.boolean  "hazardous"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "hazardous"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password"
   end
 
 end
