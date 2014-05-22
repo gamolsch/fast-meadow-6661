a6e9d9bc9eaa02a43e17d391df5f666cf85bf195
Kanhirun <kkanhirun@gmail.com>
2014-05-22 08:24:24 -0500

    chore(specs): context wrapper for tests, and changes model name from Color to Category

--- spec/models/storage_spec.rb
+++ spec/models/storage_spec.rb
@@ -14,25 +14,25 @@ describe Storage do
 		expect( Array(storage.items) ).to match_array([first_item, second_item, third_item])
 	end
 
-	xit "should have a storage capacity of 8,192" do
+	it "should have a storage capacity of 8,192" do
 		expect(Storage::Capacity).to eq(8_192)
 	end
 
-	xit "should return an array of colors" do
-		first_color, second_color = [Color.new, Color.new]
-		storage.colors << [first_color, second_color]
+	it "should return an array of categories" do
+		first_category, second_category = [Category.new, Category.new]
+		storage.colors << [first_category, second_category]
 
-		expect(storage.colors).to match_array([first_color, second_color])
+		expect(storage.colors).to match_array([first_category, second_category])
 	end
 
 	describe "#get_number_of_items" do
-		it("should return the storage size") do
-			first_item, second_item, third_item = [ Item.new, Item.new, Item.new ]
-			storage.items << [first_item, second_item, third_item]
+			it("should return the storage size") do
+				first_item, second_item, third_item = [ Item.new, Item.new, Item.new ]
+				storage.items << [first_item, second_item, third_item]
 
-			expect(storage.get_number_of_items).to eq(3)
-	  	end
-	end
+				expect(storage.get_number_of_items).to eq(3)
+		  	end
+		end
 
 	describe "#get_number_of_colors" do
 		it "should return the correct number" do
@@ -43,30 +43,33 @@ describe Storage do
 		end
 	end
 
-	describe '#get_optimization_number' do
-		it "should return 8 when recommendation is greater than 8" do
-			dozen_items = Array.new(12) { Item.new }
-			storage.items << dozen_items
+	context "assigning a code to an item belonging to color" do
 
-			expect(storage.get_optimization_number).to eq(8)
-		end
+		describe '#get_optimization_number' do
+			xit "should return 8 when recommendation is greater than 8" do
+				dozen_items = Array.new(12) { Item.new }
+				storage.items << dozen_items
 
-		it "should return 2 when storage size is 5" do
-			five_items = Array.new(5) { Item.new }
-			storage.items << five_items
+				expect(storage.get_optimization_number).to eq(8)
+			end
 
-			expect(storage.get_optimization_number).to eq(2)
-		end
+			xit "should return 2 when storage size is 5" do
+				five_items = Array.new(5) { Item.new }
+				storage.items << five_items
 
-		it "should return 1 when storage size is 1" do
-			one_item = Item.new
-			storage.items << one_item
+				expect(storage.get_optimization_number).to eq(2)
+			end
 
-			expect(storage.get_optimization_number).to eq(1)
-		end
+			xit "should return 1 when storage size is 1" do
+				one_item = Item.new
+				storage.items << one_item
 
-		it "should return 0 when storage size is 0" do
-			expect(storage.get_optimization_number).to eq(0)
+				expect(storage.get_optimization_number).to eq(1)
+			end
+
+			xit "should return 0 when storage size is 0" do
+				expect(storage.get_optimization_number).to eq(0)
+			end
 		end
 	end
 end
