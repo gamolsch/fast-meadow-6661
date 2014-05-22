e6e03b399167f94708678fd893d40077fe38a4ad
George Amolsch <gamolsch@gmail.com>
2014-05-22 13:43:51 -0500

    chore(specs): creates tests for Users

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe User do
+
+	let(:user) { User.create(first_name: "George", last_name: "Amolsch", email: "gamolsch@gmail.com", password: "1234") }
+
+
+############################################################
+# 			The following tests are ATTRIBUTE tests						#
+############################################################
+
+  it("should return correct first name") do
+  	expect(user.first_name).to eq("George")
+  end
+
+  it("should return correct last name") do
+  	expect(user.last_name).to eq("Amolsch")
+  end
+
+  it("should return correct email") do
+  	expect(user.email).to eq("gamolsch@gmail.com")
+  end
+
+  it("should return correct password") do
+  	expect(user.password).to eq("1234")
+  end
+
+  #ENDTEST
+
+############################################################
+# 			The following tests are ASSOCIATION tests					#
+############################################################
+
+
+end
