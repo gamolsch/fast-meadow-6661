74a4d0d24720d13c8e78e0de5c8ae9db705ab0f7
Kanhirun <kkanhirun@gmail.com>
2014-05-22 01:08:41 -0500

    chore(refactor): improves code readability

--- spec/models/storage_spec.rb
+++ spec/models/storage_spec.rb
@@ -1,25 +1,24 @@
 require 'spec_helper'
 
 describe Storage do
-	let(:storage) {Storage.create(name: "gatorade lab")}
+	let(:storage) {Storage.create(name: "Gatorade Lab")}
 
-	it("should return correct name of the storage") do
-		expect(storage.name).to eq("gatorade lab")
+	it("should return correct name") do
+		expect(storage.name).to eq("Gatorade Lab")
 	end
 
-	it("should return an array of items when items is called") do
-		first_item = Item.new
-		second_item = Item.new
-		third_item = Item.new
-		storage.items << ([first_item, second_item, third_item])
-		expect(storage.items.to_a).to match_array([first_item, second_item, third_item])
+	it("should return an array of items") do
+		first_item, second_item, third_item = [ Item.new, Item.new, Item.new ]
+		storage.items << [first_item, second_item, third_item]
+
+		expect( Array(storage.items) ).to match_array([first_item, second_item, third_item])
 	end
     
 	describe "#size" do
 		it("should return the storage size") do
-			storage.items << Item.create
-			storage.items << Item.create
-			storage.items << Item.create
+			first_item, second_item, third_item = [ Item.new, Item.new, Item.new ]
+			storage.items << [first_item, second_item, third_item]
+			
 			expect(storage.size).to eq(3)
 	  	end
 	end	
