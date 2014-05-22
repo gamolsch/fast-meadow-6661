533f510e35a746b1de4338eb888f077effb6b3f5
Dummy Name <you@none.com>
2014-05-21 19:18:01 -0500

    Reformatted csv file and created convert_date_format method in seed to allow seeding of database from csv file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,22 @@ def determine_if_hazardous(location_type_pair)
   location_type_pair[1] != "Hazardous" ? false : true
 end
 
+def convert_date_format(date)
+  if date = "TBD" || date = "NULL"
+    return "01/01/1900"
+  else
+    return DateTime.strptime(date, "%m/%d/%Y").strftime("%d/%m/%Y")
+  end
+end
+
 csv_text = File.read("db/mainDB-inventory.csv")
 
-CSV.parse(csv_text).each do |row|
+CSV.parse(csv_text, headers: true).each do |row|
+  p row
+  p row[4]
   split_string = split_location_string(row[0])
   is_hazardous = determine_if_hazardous(split_string)
-  p Item.create(storage: split_string[0], hazardous: is_hazardous, manufacturer: row[1], name: row[2], lot_number: row[3], manufactured_on: row[4], expired_on: row[5])
+  p Item.create(storage: split_string[0], hazardous: is_hazardous, manufacturer: row[1], name: row[2], lot_number: row[3], manufactured_on: convert_date_format(row[4]), expired_on: convert_date_format(row[5]))
 end
 
 
