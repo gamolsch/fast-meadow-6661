b294c4d2f16f10444a4dab6fa475c750fd0da9cf
Kanhirun <kkanhirun@gmail.com>
2014-05-22 01:19:01 -0500

    chore(specs): adds pending tests for future dev

--- spec/models/storage_spec.rb
+++ spec/models/storage_spec.rb
@@ -13,14 +13,25 @@ describe Storage do
 
 		expect( Array(storage.items) ).to match_array([first_item, second_item, third_item])
 	end
-    
+
+	xit "should have a storage limit of 8,192" do
+		expect(Storage::StorageLimit).to eq(8_192)
+	end
+
+	xit "should return an array of colors" do
+		first_color, second_color = [Color.new, Color.new]
+		storage.colors << [first_color, second_color]
+
+		expect(storage.colors).to match_array([first_color, second_color])
+	end
+
 	describe "#size" do
 		it("should return the storage size") do
 			first_item, second_item, third_item = [ Item.new, Item.new, Item.new ]
 			storage.items << [first_item, second_item, third_item]
-			
+
 			expect(storage.size).to eq(3)
 	  	end
-	end	
+	end
 end
 
