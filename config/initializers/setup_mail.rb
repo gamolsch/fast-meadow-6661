a2ce786eeda0f6a86cbd339119aecbc08cac76db
George Amolsch <gamolsch@gmail.com>
2014-05-22 15:54:10 -0500

    feature(mailers): sends email from the terminal
    needs to be automated and email needs to only include expired samples

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,11 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-	:address => "smtp.servername.com",
-	:port => 587,
-	:domain => "whatever.org",
-	:user_name => "username",
-	:password => "password",
+	:address => "smtp.gmail.com",
+	:port => 465,
+	:domain => "smtp.gmail.com",
+	:user_name => ENV["GMAIL_USERNAME"], #'gamolsch@gmail.com',
+	:password => ENV["GMAIL_PASSWORD"], #'0l0a0b2n4u4m0b7e9r01',
 	:authentication => "plain",
-	:enable_starttls_auto => true
+	:enable_starttls_auto => true,
+	:ssl => true
 }
