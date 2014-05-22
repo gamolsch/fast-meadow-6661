483e4376dde3be15bab13b74c5fa5583e65e6d99
George Amolsch <gamolsch@gmail.com>
2014-05-22 12:44:15 -0500

    feature(mailer): adds expiration method

--- app/mailers/expiration_mailer.rb
+++ app/mailers/expiration_mailer.rb
@@ -3,6 +3,15 @@ class ExpirationMailer < ActionMailer::Base
 	#ActionMailer::Base.default :from => 'default@production-server.com'
   
   def expiration_confirmation(user)
-  	mail :to => user.email, :subject => "Expired Items"
-  end
+   @items = Item.All
+    time = Time.now
+    @user = user
+
+	  @items.each do |item|
+			if time > item.expired_on
+				mail :to => @user.email, :subject => "Expired Items"
+			else
+				"not expired"
+			end
+	  end
 end
