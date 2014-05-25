b2eb87f5fcabcd33b1e532180a751342e946aae0
Kel Kanhirun <kkanhirun@gmail.com>
2014-05-25 15:26:07 -0500

    chore(comments): removes legacy code and clarifies list

--- angular-app/app/js/controllers.js
+++ angular-app/app/js/controllers.js
@@ -1,37 +1,16 @@
 'use strict';
 
 /* Controllers */
-// var searchApp =
+
 angular.module('searchApp')
 .factory('Item', function ($resource) {
 	return $resource('/api/items/:itemID');
 });
 
-// NOT IN EFFECT
 angular.module('searchApp')
 .controller('ItemListController', function ($scope, Item) {
 
-	// $scope.orderProp = 'name';
 	$scope.items = Item.query();
-
-	// $scope.create = function(manufacturer, name, lot_number, manufactured_on, expired_on, unit_of_measure) {
-	// 	Item.save({manufacturer: manufacturer, name: name, lot_number: lot_number, manufactured_on: manufactured_on, expired_on: expired_on, unit_of_measure: unit_of_measure },
-	// 		function(item) {
-	// 			$scope.items.push(item);
-	// 	});
-	// };
-
-	// $scope.delete = function(index) {
-	// 	Item.delete({itemID: $scope.items[index].id}, function(){
-	// 		$scope.items.splice(index, 1);
-	// 	});
-	// };
+	// TODO: fix default order
+	// $scope.orderProp = 'manufacturer';
 });
-
-// searchApp.factory("Item", function($resource) {
-//   return $resource("/api"); // it should come packaged with RESTful routes, right?
-// })
-
-// searchApp.controller('ItemListController', ['$scope', function($scope, Item) {
-//   $scope.items = Item.query();
-// }])
