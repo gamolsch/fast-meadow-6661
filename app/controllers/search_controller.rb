2542fe7456bba17de4165b5b758927a6de72fe8e
Kel Kanhirun <kkanhirun@gmail.com>
2014-05-26 17:07:38 -0500

    chore(build): builds angular into public files and resolves issues

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,9 @@
 class SearchController < ApplicationController
 
+  def main
+    render "ng-search", layout: false
+  end
+
   # GET /items.json
   def index
     render json: Item.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
-   # root 'welcome#index'
-
-   # resources :storages
-   # resources :items
-   # resources :transaction
+   root 'welcome#index'
+   resources :storages
+   resources :items
+   resources :transaction
 
   # # Used for testing purposes
   # get '/api', to: 'api#index'
 
-  # get '/home', to: 'users#home'
+  get '/home', to: 'users#home'
   # get '/storages', to: 'storages#index'
   # get '/storages/1', to: 'storages#show'
   # get '/items/new', to: 'items#new'
   # get '/items/1', to: 'items#show'
 
   # #AngularJS entry-point:
+  get '/search', to: 'search#main'
   resources :search, path: 'api/items'
    # get 'api/search', to: 'application#search'
 
