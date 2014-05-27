384f0fb28b187bee0fb5bd24f8d00d54d0fb5905
Kel Kanhirun <kkanhirun@gmail.com>
2014-05-26 21:09:14 -0500

    chore(refactor): cleans up routes to focus on mvp

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,10 @@
 Rails.application.routes.draw do
-   root 'welcome#index'
-   resources :storages
-   resources :items
-   resources :transaction
+  root 'users#home'
 
-  # # Used for testing purposes
-  # get '/api', to: 'api#index'
-
-  get '/home', to: 'users#home'
-  # get '/storages', to: 'storages#index'
-  # get '/storages/1', to: 'storages#show'
-  # get '/items/new', to: 'items#new'
-  # get '/items/1', to: 'items#show'
-
-  # #AngularJS entry-point:
-  get '/search', to: 'search#main'
+  resources :storages
+  resources :items
+  resources :transaction
   resources :search, path: 'api/items'
-   # get 'api/search', to: 'application#search'
 
-
-  #get "api/ping" => "application#ping"
-
-
+  get '/search', to: 'search#main'
 end
