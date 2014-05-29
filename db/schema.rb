ee31aaea36cedc96445f0caaa02401385ae09bfb
gamolsch <gamolsch@gmail.com>
2014-05-28 21:46:08 -0500

    feature: adds amount to item field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140526143336) do
+ActiveRecord::Schema.define(version: 20140529022516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20140526143336) do
     t.datetime "updated_at"
     t.integer  "category_id"
     t.string   "code"
+    t.integer  "amount"
   end
 
   create_table "storages", force: true do |t|
